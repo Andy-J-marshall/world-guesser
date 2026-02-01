e77d6bc5cc84309691c473be82f81a615182fd05
andrew marshall <andrew.marshall@thegymgroup.com>
2026-02-01 21:58:56 +0000

    fix: hide empty error message

--- src/components/validation/BasicValidation.tsx
+++ src/components/validation/BasicValidation.tsx
@@ -1,18 +1,22 @@
 interface BasicValidationProps {
-    duplicateGuess: boolean;
-    knownCountry: boolean;
+  duplicateGuess: boolean;
+  knownCountry: boolean;
 }
 
 function BasicValidation(props: BasicValidationProps) {
-    const duplicateGuess = props.duplicateGuess;
-    const knownCountry = props.knownCountry;
+  const duplicateGuess = props.duplicateGuess;
+  const knownCountry = props.knownCountry;
 
-    return (
-        <div style={{ color: '#F66B0E' }} id='invalid-guess-feedback'>
-            {duplicateGuess && <p>You've already tried that country!</p>}
-            {!knownCountry && <p>Enter a valid country name</p>}
-        </div>
-    )
+  if (!duplicateGuess && knownCountry) {
+    return null;
+  }
+
+  return (
+    <div style={{ color: "#F66B0E" }} id="invalid-guess-feedback">
+      {duplicateGuess && <p>You've already tried that country!</p>}
+      {!knownCountry && <p>Enter a valid country name</p>}
+    </div>
+  );
 }
 
 export default BasicValidation;
